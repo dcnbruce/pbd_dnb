e8d84875a6447969db87675f4d7a728925b9cbf6
Douglas Nanka-Bruce <dcnbruce@yahoo.com>
2017-06-29 12:58:27 +0100

    handle a document

--- spellchecker/spellcheck_test.py
+++ spellchecker/spellcheck_test.py
@@ -10,12 +10,21 @@ class TestspellChecker(unittest.TestCase):
 
     def test_spell_Checker(self):
         self.assertTrue(self.spellChecker.check_word('zygotic'))
-        self.assertFalse(self.spellChecker.check_words('zygotic mistasdas elementary'))
-        self.assertTrue(self.spellChecker.check_words('our first correct sentence'))
-		# handle case sensitivity
-        self.assertTrue(self.spellChecker.check_words('Our first correct sentence'))
+        failed_words = self.spellChecker.check_words('zygotic mistasdas elementary')
+        self.assertEquals(1, len(failed_words))
+        self.assertEquals('mistasdas', failed_words[0])
+        self.assertEquals(0, len(self.spellChecker.check_words('our first correct sentence')))
+        # handle case sensitivity
+        self.assertEquals(0, len(self.spellChecker.check_words('Our first correct sentence')))
         # handle full stop
-        self.assertTrue(self.spellChecker.check_words('Our first correct sentence.'))
+        self.assertEquals(0, len(self.spellChecker.check_words('Our first correct sentence.')))
+        failed_words = self.spellChecker.check_words('zygotic mistasdas spelllleeeing elementary')
+        self.assertEquals(2, len(failed_words))
+        self.assertEquals('mistasdas', failed_words[0])
+        self.assertEquals('spelllleeeing', failed_words[1])
 
+        # more bugs because the spell checker doesn't spell check itself correctly  "21 entries not correct  dictionary words" # need to be lower
+        self.assertEqual(0, len(self.spellChecker.check_document('spell.words')))
+		
 if __name__ == '__main__':
     unittest.main()
--- spellchecker/spellchecker.py
+++ spellchecker/spellchecker.py
@@ -4,18 +4,34 @@ class spellChecker(object):
 	
 	def load_words(self, file_name):
 		self.words = open(file_name).readlines()
-		self.words = map(lambda x: x.strip(), self.words)
+		self.words = map(lambda x: x.strip().lower(), self.words)
 
+	def load_file(self, file_name):
+		lines = open(file_name).readlines()
+		lines = map(lambda x: x.strip(), lines)
+		return lines
+		
 	def check_word(self, word) :
-			return word.strip('.').lower() in self.words
+		return word.strip('.').lower() in self.words
 			
 	def check_words(self, sentence):
 		words_to_check = sentence.split(' ')
+		failed_words = []
 		for word in words_to_check:
 			if not self.check_word(word):
 				print('Word is misspelt : ' + word)
-				return False
-		return True
+				failed_words.append(word)
+		return failed_words
+		
+	def check_document(self, file_name):
+		self.sentences = self.load_file(file_name)
+		failed_words_in_sentences = []
+		index = 0
+		for sentence in self.sentences:
+			failed_words_in_sentences.extend(self.check_words(sentence))
+			index = index + 1
+		return failed_words_in_sentences
+ 
 
 
 if __name__=='__main__':
